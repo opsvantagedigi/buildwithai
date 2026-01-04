c787feec04fb9abf734aaca121c15eed6bfe3ae3
opsvantagedigi <ajay.sidal@opsvantagedigital.online>
2026-01-04 15:53:28 +1300

    chore(style): apply stylelint --fix

---
+++ .stylelintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-tailwindcss'
+  ],
+  ignoreFiles: ['**/node_modules/**', '.next/**', 'tmp/**'],
+  rules: {},
+};
